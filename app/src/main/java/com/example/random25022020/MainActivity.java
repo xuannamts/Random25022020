e08527840abb9d19dbcb4e42adb2092fbc0db98d
xuannamts <nguyenxuannamts98@gmail.com>
2020-03-19 10:05:59 +0700

    test random

--- app/src/main/java/com/example/random25022020/MainActivity.java
+++ app/src/main/java/com/example/random25022020/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,7 +17,22 @@ public class MainActivity extends AppCompatActivity {
         // lam tron
 //        Double value1 = Math.ceil(a);
         // lam tron xuong
-        double value2 = Math.floor(a);
-        Log.d("BBB",String.valueOf(value2));
+//        double value2 = Math.floor(a);
+//        Log.d("BBB",String.valueOf(value2));
+        // can bac hai
+//        double b = 9;
+//        double value = Math.sqrt(b);
+//        Log.d("BBB",String.valueOf(value));
+//        double value = Math.random();
+//        Log.d("BBB",String.valueOf(value));
+
+        // Random 0-5
+//        Random random = new Random();
+//        for (int i = 0; i < 100 ; i++){
+//            int value = random.nextInt(7) - random.nextInt(7);
+//            Log.d("BBB",String.valueOf(value));
+//        }
+
+
     }
 }
